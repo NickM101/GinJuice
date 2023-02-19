fd2b4781ac3dc548d282cdf94ba1fb7441234046
Nick Munene <nickmunene101@gmail.com>
2023-02-19 21:57:18 +0300

    fixing rails navigation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ginjuice/widgets/rail_button.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,8 +9,119 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final int paneProportion = 100;
+
+  final List<String> items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+    'Item 6',
+    'Item 7',
+    'Item 8',
+    'Item 9',
+    'Item 10',
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+    'Item 6',
+    'Item 7',
+    'Item 8',
+    'Item 9',
+    'Item 10',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Home page'));
+    return Scaffold(
+      body: Row(
+        children: [
+          // Left Column
+          Container(
+            width: 100,
+            // color: Colors.blueGrey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RailButton(
+                    text: 'Shots',
+                    icon: Icons.accessibility_rounded,
+                    onPressed: () => {}),
+                const SizedBox(
+                  height: 50,
+                ),
+                RailButton(
+                    text: 'Shots',
+                    icon: Icons.accessibility_rounded,
+                    onPressed: () => {}),
+                const SizedBox(
+                  height: 50,
+                ),
+                RailButton(
+                    text: 'Shots',
+                    icon: Icons.accessibility_rounded,
+                    onPressed: () => {}),
+                // ElevatedButton(
+                //   onPressed: () {},
+                //   child: Text('Button 1'),
+                // ),
+                // ElevatedButton(
+                //   onPressed: () {},
+                //   child: Text('Button 2'),
+                // ),
+                // ElevatedButton(
+                //   onPressed: () {},
+                //   child: Text('Button 3'),
+                // ),
+              ],
+            ),
+          ),
+          // Right Column
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(items[index]),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Flex(
+  //     direction: Axis.horizontal,
+  //     children: [
+  //       Flexible(
+  //         flex: 2 - paneProportion,
+  //         child: PaneLeft(),
+  //       ),
+  //       Flexible(flex: paneProportion, child: PaneRight())
+  //     ],
+  //   );
+  // }
+  //
+  // Widget PaneLeft() {
+  //   return Column(
+  //     mainAxisAlignment: MainAxisAlignment.center,
+  //     // crossAxisAlignment: CrossAxisAlignment.start,
+  //     children: const [
+  // TrapezoidButton(text: 'Shot', icon: Icons.adb_sharp),
+  //     ],
+  //   );
+  // }
+  //
+  // Widget PaneRight() {
+  //   return Container(
+  //     color: Colors.red,
+  //   );
+  // }
 }
--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -5,6 +5,60 @@ class SearchPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Search page'));
+    return Center(
+      child: Container(
+        color: Colors.amber,
+        width: 300,
+        height: 300,
+        child: CustomPaint(
+          painter: LearningPainting(),
+          // child: Stack(
+          //   children: const [
+          //     Positioned(
+          //       top: 32,
+          //       left: 12,
+          //       child: Icon(Icons.accessibility_rounded),
+          //     ),
+          //     Positioned(
+          //       top: 60,
+          //       left: 12,
+          //       child: Text(
+          //         'Da Vinci',
+          //         style: TextStyle(
+          //           color: Colors.white,
+          //           fontSize: 16,
+          //         ),
+          //       ),
+          //     ),
+          //   ],
+          // ),
+        ),
+      ),
+    );
   }
+}
+
+class LearningPainting extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..strokeWidth = 5
+      ..color = Colors.black
+      ..style = PaintingStyle.fill
+      ..strokeCap = StrokeCap.round
+      ..strokeJoin = StrokeJoin.round
+      ..strokeMiterLimit = 60;
+
+    final path = Path()
+      ..moveTo(0, 0)
+      ..lineTo(0, size.height)
+      ..lineTo(size.width * 0.2, size.height * 0.8)
+      ..lineTo(size.width * 0.2, size.height * 0.2)
+      ..close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(LearningPainting oldDelegate) => false;
 }
