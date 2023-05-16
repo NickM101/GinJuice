db5abd304e94db102fd1b0c264fa16cd7175cb42
Nick_Munene <nickmunene101@gmail.com>
2023-05-16 12:28:22 +0300

    Apply typography

--- lib/config/themes/text_theme.dart
+++ lib/config/themes/text_theme.dart
@@ -2,80 +2,80 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 final textTheme = TextTheme(
-  displayLarge: GoogleFonts.robotoCondensed(
-    fontWeight: FontWeight.w700,
+  displayLarge: GoogleFonts.inter(
+    fontWeight: FontWeight.w400,
     fontSize: 57,
     height: 64 / 57,
     letterSpacing: -0.25,
   ),
-  displayMedium: GoogleFonts.robotoCondensed(
-    fontWeight: FontWeight.w600,
+  displayMedium: GoogleFonts.inter(
+    fontWeight: FontWeight.w400,
     fontSize: 45,
     height: 52 / 45,
   ),
-  displaySmall: GoogleFonts.robotoCondensed(
+  displaySmall: GoogleFonts.inter(
     fontWeight: FontWeight.w400,
     fontSize: 36,
     height: 44 / 36,
   ),
-  headlineLarge: GoogleFonts.robotoCondensed(
-    fontWeight: FontWeight.w700,
+  headlineLarge: GoogleFonts.inter(
+    fontWeight: FontWeight.w400,
     fontSize: 32,
     height: 40 / 32,
   ),
-  headlineMedium: GoogleFonts.robotoCondensed(
-    fontWeight: FontWeight.w600,
+  headlineMedium: GoogleFonts.inter(
+    fontWeight: FontWeight.w400,
     fontSize: 28,
     height: 36 / 28,
   ),
-  headlineSmall: GoogleFonts.robotoCondensed(
+  headlineSmall: GoogleFonts.inter(
     fontWeight: FontWeight.w400,
     fontSize: 24,
     height: 32 / 24,
   ),
-  titleLarge: GoogleFonts.robotoCondensed(
+  titleLarge: GoogleFonts.inter(
     fontWeight: FontWeight.w700,
     fontSize: 22,
     height: 28 / 22,
   ),
-  titleMedium: GoogleFonts.robotoCondensed(
+  titleMedium: GoogleFonts.inter(
     fontWeight: FontWeight.w600,
     fontSize: 16,
     height: 24 / 16,
     letterSpacing: 0.1,
   ),
-  titleSmall: GoogleFonts.robotoCondensed(
+  titleSmall: GoogleFonts.inter(
     fontWeight: FontWeight.w600,
     fontSize: 14,
     height: 20 / 14,
     letterSpacing: 0.1,
   ),
-  labelLarge: GoogleFonts.robotoCondensed(
-    fontWeight: FontWeight.w600,
+  labelLarge: GoogleFonts.inter(
+    fontWeight: FontWeight.w700,
     fontSize: 14,
     height: 20 / 14,
   ),
-  labelMedium: GoogleFonts.robotoCondensed(
-    fontWeight: FontWeight.w500,
+  labelMedium: GoogleFonts.inter(
+    fontWeight: FontWeight.w700,
     fontSize: 12,
     height: 16 / 12,
   ),
-  labelSmall: GoogleFonts.robotoCondensed(
-    fontWeight: FontWeight.w400,
+  labelSmall: GoogleFonts.inter(
+    fontWeight: FontWeight.w700,
     fontSize: 11,
     height: 16 / 11,
   ),
-  bodyLarge: GoogleFonts.robotoCondensed(
+  bodyLarge: GoogleFonts.inter(
     fontWeight: FontWeight.w400,
     fontSize: 16,
     height: 24 / 16,
   ),
-  bodyMedium: GoogleFonts.robotoCondensed(
+  bodyMedium: GoogleFonts.inter(
     fontWeight: FontWeight.w400,
     fontSize: 14,
     height: 20 / 14,
   ),
-  bodySmall: GoogleFonts.robotoCondensed(
+  bodySmall: GoogleFonts.inter(
     fontWeight: FontWeight.w400,
     fontSize: 12,
     height: 16 / 12,
